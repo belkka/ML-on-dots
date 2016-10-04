b499f3924a4dbb9bf1c0ca675185c445f6c40507
Ulyanich Michael <ulianich_mihail@ukr.net>
2016-10-05 00:18:18 +0300

    Добавлены условия и решения задач про ирисы. Изменено условие задач про изображения цифр.

--- pendigits/solutions/bad Solution 1.py
+++ pendigits/solutions/bad Solution 1.py
@@ -3,13 +3,16 @@ import numpy as np
 import random
 
 
+K = 10
+
+
 with open("input.txt") as raw_data:
     X = np.loadtxt(raw_data, dtype=int)
 
-clusters = [list() for __ in xrange(10)]
+clusters = [list() for __ in xrange(K)]
 
 for (i, x) in enumerate(X):
-    clusters[random.randint(0, 9)].append(i)
+    clusters[random.randint(0, K - 1)].append(i)
 
-for i in xrange(10):
+for i in xrange(K):
     print " ".join(map(str, clusters[i]))
